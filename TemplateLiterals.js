06e97cf8b1b580006a81d448f5b02fffc10e95be
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2023-02-28 20:06:45 -0300

    Create TemplateLiterals.js

---
+++ TemplateLiterals.js
@@ -0,0 +1,6 @@
+const name = 'João';
+const age = 25;
+
+const message = `Olá, meu nome é ${name} e eu tenho ${age} anos de idade.`;
+
+console.log(message); // Output: "Olá, meu nome é João e eu tenho 25 anos de idade."
